c2077519ee8af5d507df2585017f80805a773c56
angelostori <angelostori13@gmail.com>
2025-11-20 17:58:39 +0100

    add getFlag function to display country flags based on language

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,32 @@ export default function App() {
       .catch(err => console.log(err));
   }
 
+  function getFlag(lang) {
+    const flags = {
+      it: <img width="25" height="25" src="https://img.icons8.com/color/48/italy.png" alt="italy" />,
+      en: <img width="25" height="25" src="https://img.icons8.com/color/48/great-britain.png" alt="great-britain" />,
+      fr: <img width="25" height="25" src="https://img.icons8.com/color/48/france.png" alt="france" />,
+      es: <img width="25" height="25" src="https://img.icons8.com/color/48/spain.png" alt="spain" />,
+      de: <img width="25" height="25" src="https://img.icons8.com/color/48/germany.png" alt="germany" />,
+      ja: <img width="25" height="25" src="https://img.icons8.com/color/48/japan.png" alt="japan" />,
+      ko: <img width="25" height="25" src="https://img.icons8.com/color/48/south-korea.png" alt="south-korea" />,
+      pt: <img width="25" height="25" src="https://img.icons8.com/color/48/portugal.png" alt="portugal" />,
+      ru: <img width="25" height="25" src="https://img.icons8.com/color/48/russian-federation.png" alt="russian" />,
+      nl: <img width="25" height="25" src="https://img.icons8.com/color/48/netherlands.png" alt="netherlands" />,
+      sv: <img width="25" height="25" src="https://img.icons8.com/color/48/sweden.png" alt="sweden" />,
+      hi: <img width="25" height="25" src="https://img.icons8.com/color/48/india.png" alt="india" />,
+      cn: <img width="25" height="25" src="https://img.icons8.com/color/48/china.png" alt="china" />,
+      zh: <img width="25" height="25" src="https://img.icons8.com/color/48/china.png" alt="china" />,
+      sl: <img width="25" height="25" src="https://img.icons8.com/color/48/slovenia.png" alt="slovenia" />,
+      sh: <img width="25" height="25" src="https://img.icons8.com/color/48/zimbabwe.png" alt="zimbabwe" />,
+      he: <img width="25" height="25" src="https://img.icons8.com/color/48/israel.png" alt="israel" />,
+      el: <img width="25" height="25" src="https://img.icons8.com/color/48/greece.png" alt="greece" />,
+      th: <img width="25" height="25" src="https://img.icons8.com/color/48/thailand.png" alt="thailand" />
+    }
+
+    return flags[lang] || "🏳️"
+  }
+
   return (
     <div className="container py-4">
       <h1>Boolflix</h1>
@@ -33,7 +59,7 @@ export default function App() {
           <div key={movie.id}>
             <h3>{movie.title}</h3>
             <p>Titolo originale: {movie.original_title}</p>
-            <p>Lingua: {movie.original_language}</p>
+            <p>Lingua: {movie.original_language.toUpperCase()} {getFlag(movie.original_language)}</p>
             <p>Voto: {movie.vote_average}</p>
           </div>
         ))}
